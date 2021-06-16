6a69ab872a455501401db3156a829ccb54b861d3
Brian Firestone <bfirestone2339@gmail.com>
2021-06-15 18:53:09 -0700

    initial commit

---
+++ algos/arrays/max_profit.js
@@ -0,0 +1,13 @@
+const maxProfit = (prices) => {
+    let profit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i - 1] < prices[i]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    
+    return profit;
+};
+
+prices = [7,1,5,3,6,4]
