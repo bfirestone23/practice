a79d77bc71101dfd7d7b2862ad6ca6d826abe147
Brian Firestone <bfirestone2339@gmail.com>
2021-06-21 17:36:01 -0700

    Add simplified solution

--- sorting/selection_sort.js
+++ sorting/selection_sort.js
@@ -1,25 +1,24 @@
-// SELECTION SORT
-const findMinAndRemove = arr => {
-    let min = arr[0]
-    let minIndex = 0
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i]
-            minIndex = i
+const selectionSort = arr => {
+    // begin outer loop at first element
+    for(let i = 0; i < arr.length; i++) {
+        // set current lowest to first element (index)
+        let lowest = i;
+        // begin inner loop at next element after i
+        for (let j = i + 1; j < arr.length; j++) {
+            // if this element is less than lowest, set new lowest
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
         }
+        // once inner loop has completed, swap values (if i is not he same as lowest)
+        if (i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+        
     }
-
-    arr.splice(minIndex, 1)
-    return min;
+    return arr;
 }
 
-const selectionSort = unsortedArr => {
-    let sortedArr = []
-
-    while (unsortedArr.length > 0) {
-        sortedArr.push(findMinAndRemove(unsortedArr))
-    }
-
-    return sortedArr;
-}
+console.log(selectionSort([3,2,1,29,5,89]))
