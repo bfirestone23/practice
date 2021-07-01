a7d839f9451634140f8ba30ccaf9aa096ab14815
Brian Firestone <bfirestone2339@gmail.com>
2021-06-30 18:45:59 -0700

    Update solution

--- data_structures/SLL.js
+++ data_structures/SLL.js
@@ -1,48 +1,82 @@
 class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
+    constructor(data = null, next = null){
+        this.val = data;
+        this.next = next;
     }
 }
 
-class SinglyLinkedList{
-    constructor(){
+class LinkedList{
+    constructor() {
         this.head = null;
-        this.tail = null;
-        this.length = 0;
     }
 
-    push(val){
-        let node = new Node(val);
-        if (!this.head) {
-            this.head = node;
-            this.tail = this.head;
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let count = 0;
+        let node = this.head;
+
+        while (node) {
+            count++;
+            node = node.next;
+        }
+
+        return count;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) return null;
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) return;
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) return;
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        let prev = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            prev = node;
+            node = node.next;
+        }
+
+        prev.next = null;
+    }
+
+    insertLast(data) {
+        let last = this.getLast();
+
+        if (last) {
+            last.next = new Node(data);
         } else {
-            this.tail.next = node;
-            this.tail = node;
+            this.head = new Node(data);
         }
-        this.length++;
-        return this;
-    }
-
-    pop(){
-        if (!this.head) {
-            return undefined;
-        }
-        
-        let current = this.head;
-        let newTail = current;
-
-        while(current.next) {
-            newTail = current;
-            current = current.next;
-        }
-
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        return current;
     }
 }
-
-debugger;
